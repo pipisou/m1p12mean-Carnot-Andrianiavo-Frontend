eb8952f7acc2b0222e0931d685a95d582758aec8
Carnot <mireillamireilla9@gmail.com>
2025-03-10 10:40:37 +0300

    front

--- Projet final/backend/models/Article.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const articleSchema = new mongoose.Schema({
-    nomArticle: { type: String, required: true },
-    categorie: { type: mongoose.Schema.Types.ObjectId, ref: 'Categorie', required: true }, // Référence vers Categorie
-}, { timestamps: true });
-
-module.exports = mongoose.model('Article', articleSchema);
--- Projet final/backend/models/Manager.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const managerSchema = new mongoose.Schema({
-    nom: { type: String, default: "manager" },
-    email: { type: String, required: true, unique: true },
-    motDePasse: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Manager', managerSchema);
